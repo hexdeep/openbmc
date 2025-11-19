4462b9efe78c40d3d7ac469a3d8134bc3973428b
2892709432@qq.com <2892709432@qq.com>
2025-11-19 11:07:15 +0800

    完善前端日志

--- frontend/src/views/LogList.vue
+++ frontend/src/views/LogList.vue
@@ -2,6 +2,7 @@
 import {request} from '@/utils/axios';
 import type {List} from '@/utils/utils';
 import {useUrlSearchParams} from '@vueuse/core';
+import dayjs from 'dayjs';
 import {ref, watchEffect} from 'vue';
 import {useI18n} from 'vue-i18n';
 
@@ -19,6 +20,8 @@ const params = useUrlSearchParams<any>('history')
 const { t } = useI18n({ messages: {
   zh: {
     logListTitle: '日志列表',
+    method: '请求方法',
+    status: '状态码',
   },
 } })
 
@@ -32,6 +35,16 @@ const statusTypeMap: Record<any, any> = {
   401: 'warning',
 }
 
+const methodTypeMap: Record<any, any> = {
+  GET: "success",
+  POST: "primary",
+  PUT: "warning",
+  DELETE: "danger",
+  PATCH: "info",
+  OPTIONS: "info",
+  HEAD: "info",
+}
+
 </script>
 
 <template>
@@ -40,10 +53,26 @@ const statusTypeMap: Record<any, any> = {
       <div class="text-lg">
         {{t('logListTitle')}}
       </div>
+      <div class="flex gap-4">
+        <el-select v-model="params.method" :placeholder="t('method')" class="!w-48">
+          <el-option :label="t('none')" value="" />
+          <el-option v-for="v in ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS']" :key="v" :label="v" :value="v" />
+        </el-select>
+        <el-select v-model="params.status" :placeholder="t('status')" class="!w-48">
+          <el-option :label="t('none')" :value="0" />
+          <el-option v-for="v in [200, 401, 500]" :key="v" :label="v" :value="v" />
+        </el-select>
+      </div>
       <el-table :data="logs.data">
-        <el-table-column :label="t('createdAt')" prop="createdAt" />
+        <el-table-column :label="t('createdAt')" prop="createdAt" :formatter="({createdAt}) => dayjs(createdAt).format('YYYY-MM-DD hh:mm:ss')" />
         <el-table-column :label="t('clientIp')" prop="clientIp" />
-        <el-table-column :label="t('method')" prop="method" />
+        <el-table-column :label="t('method')" prop="method">
+          <template #default="{ row: { method } }">
+            <el-tag :type="methodTypeMap[method]">
+              {{method}}
+            </el-tag>
+          </template>
+        </el-table-column>
         <el-table-column :label="t('path')" prop="path" />
         <el-table-column :label="t('status')">
           <template #default="{ row: { status } }">
